8ff4458e271782d9a6055c75345260be2f7e557a
Ashwin Singh Tomar <makert40@gmail.com>
2015-10-28 17:17:58 +0530

    201510281717

--- www/js/app.js
+++ www/js/app.js
@@ -61,6 +61,22 @@ angular.module('ionicApp', ['ionic','ngCordova','ngIOS9UIWebViewPatch','starter.
         }
       }
     })
+	.state('eventmenu.getinformation', {
+      url: "/getinformation",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/getinformation.html",
+        }
+      }
+    })
+	.state('eventmenu.emailus', {
+      url: "/emailus",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/emailus.html",
+        }
+      }
+    })
     .state('eventmenu.attendees', {
       url: "/attendees",
       views: {
@@ -76,45 +92,45 @@ angular.module('ionicApp', ['ionic','ngCordova','ngIOS9UIWebViewPatch','starter.
 
 .directive('map', function() {
 	return {
-    restrict: 'E',
-    scope: {
-      onCreate: '&'
-    },
-    link: function ($scope, $element, $attr) {
-      function initialize() {
-		var myLatLng = {lat: 27.9769145, lng: -82.5590481};
-        var mapOptions = {
-          center: new google.maps.LatLng(27.9769145, -82.5590481),
-          zoom: 16,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map($element[0], mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatLng,
-			map: map,
-			label: "A",
-			content:"Hello World!"
-	  	});
-		var infowindow = new google.maps.InfoWindow({
-		  content:"5424 Ginger Cove Dr"
-		});
-		infowindow.open(map,marker);
-		
-        $scope.onCreate({map: map});
-
-        // Stop the side bar from dragging when mousedown/tapdown on the map
-        google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
-          e.preventDefault();
-          return false;
-        });
-      }
-
-      if (document.readyState === "complete") {
-        initialize();
-      } else {
-        google.maps.event.addDomListener(window, 'load', initialize);
-      }
-    }
-  }
+		restrict: 'E',
+		scope: {
+		  onCreate: '&'
+		},
+		link: function ($scope, $element, $attr) {
+		  function initialize() {
+			var myLatLng = {lat: 27.9769145, lng: -82.5590481};
+			var mapOptions = {
+			  center: new google.maps.LatLng(27.9769145, -82.5590481),
+			  zoom: 16,
+			  mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			var map = new google.maps.Map($element[0], mapOptions);
+			var marker = new google.maps.Marker({
+				position: myLatLng,
+				map: map,
+				label: "A",
+				content:"Hello World!"
+			});
+			var infowindow = new google.maps.InfoWindow({
+			  content:"5424 Ginger Cove Dr"
+			});
+			infowindow.open(map,marker);
+			
+			$scope.onCreate({map: map});
+	
+			// Stop the side bar from dragging when mousedown/tapdown on the map
+			google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
+			  e.preventDefault();
+			  return false;
+			});
+		  }
+	
+		  if (document.readyState === "complete") {
+			initialize();
+		  } else {
+			google.maps.event.addDomListener(window, 'load', initialize);
+		  }
+		}
+  	}
 })
 
