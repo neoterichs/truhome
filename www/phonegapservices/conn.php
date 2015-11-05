2df7118e9a9df2a330f0cc19a7aa6d4bcc493ef4
Ashwin Singh Tomar <makert40@gmail.com>
2015-11-05 17:15:46 +0530

    201511051715

--- www/phonegapservices/conn.php
+++ www/phonegapservices/conn.php
@@ -1,8 +1,9 @@
 <?php 
 session_start();
 error_reporting(E_ALL ^ E_NOTICE);
-$db = pg_connect("host=45.79.145.23 port=5432 dbname=truhome user=postgres password=Nislinode$1")
+$db = pg_connect("host=45.79.212.193 port=5432 dbname=truhome user=postgres password=Nislinode$1")
 or die("unable");
+$ip = "http://45.79.212.193/truhome.co/public_html/phonegapservices/";
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
